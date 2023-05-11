465ae747e8a326987625f5c5c85cd3516bb35de5
Krasimir <krasimir.genchev@softwaregroup.com>
2023-05-11 19:59:40 +0300

    setup mongo atlas

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,8 @@
 module.exports = {
-  googleClientID:
+    googleClientID:
     '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI:
-    'mongodb+srv://readonly:0Wc2It8kdXQ8INbh@cluster0.ffbere1.mongodb.net/advnode?retryWrites=true&w=majority',
-  cookieKey: '123123123',
+    googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
+    mongoURI:
+    'mongodb+srv://krasimirhg:mG40VsioiqgAOdkY@cluster0.stlcvnw.mongodb.net/NODEADVANCEDCON?retryWrites=true&w=majority',
+    cookieKey: '123123123'
 };
