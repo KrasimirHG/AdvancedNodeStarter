c84ddeeed733ad175e0b49cf055a98e817ce6274
Krasimir <krasimir.genchev@softwaregroup.com>
2023-05-12 18:14:06 +0300

    override mongoose query

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -4,35 +4,51 @@ const requireLogin = require('../middlewares/requireLogin');
 const Blog = mongoose.model('Blog');
 
 module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
+    app.get('/api/blogs/:id', requireLogin, async(req, res) => {
+        const blog = await Blog.findOne({
+            _user: req.user.id,
+            _id: req.params.id
+        });
+
+        res.send(blog);
     });
 
-    res.send(blog);
-  });
+    app.get('/api/blogs', requireLogin, async(req, res) => {
+        const redis = require('redis');
+        const redisUrl = 'redis://127.0.0.1:6379';
+        const client = redis.createClient(redisUrl);
+        const util = require('util');
+        client.get = util.promisify(client.get);
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+        const cachedBlogs = await client.get(req.user.id);
 
-    res.send(blogs);
-  });
+        if (cachedBlogs) {
+            console.log('SERVING FROM CACHE');
+            return res.send(JSON.parse(cachedBlogs));
+        }
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+        const blogs = await Blog.find({ _user: req.user.id });
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
+        console.log('SERVING FROM MONGODB');
+        res.send(blogs);
+
+        client.set(req.user.id, JSON.stringify(blogs));
     });
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+    app.post('/api/blogs', requireLogin, async(req, res) => {
+        const { title, content } = req.body;
+
+        const blog = new Blog({
+            title,
+            content,
+            _user: req.user.id
+        });
+
+        try {
+            await blog.save();
+            res.send(blog);
+        } catch (err) {
+            res.send(400, err);
+        }
+    });
 };
---
+++ services/cache.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function() {
+    console.log('Run a Query');
+
+    return exec.apply(this, arguments);
+};
